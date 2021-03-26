3adab75987784695c817c8d6469f5b39e6b898be
123xenos321 <61549131+123xenos321@users.noreply.github.com>
2021-03-26 18:43:35 +0100

    Update World.py

--- src/com/company/World.py
+++ src/com/company/World.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-.
 """
 Created on Fri Mar 26 17:40:52 2021
 
@@ -6,4 +6,62 @@ Created on Fri Mar 26 17:40:52 2021
 """
 
 import SimpleAvida
-
+"""Cell == Programm!"""
+"""Thinking about how to implement the Avida-World"""
+#%%
+"""Class World is the AvidaWorld, how big it is, calls the cpuemulator, counts 
+the time the cells are alive, gives them resources
+"""
+class World:
+    def __init__(self,emulator):
+        self.emulator = emulator
+    """How many cells can be alive at the same time"""
+    def Worldsize(size):
+    #TODO
+        pass
+    """Call the function"""
+    def Functioncall(instructionlist):
+    #TODO    
+        pass 
+    """kill the cell after a certain amount of time"""
+    def Killswitch(time,cell):
+    #TODO
+        pass
+    """call the h-divide and h-allocate function and let the parent cell give
+    birth to a new programm"""
+    def Replication(cell):
+    #TODO
+        pass
+  #%%
+"""Class mutation is responsible for every mutation factor of our programms 
+when they are replicating.
+"""  
+class Mutation:
+    def __init__(self,time,emulator,factor):
+        self.emulator = emulator
+        self.time = time
+        self.mutationfactor = factor
+        
+    def mutation(cell):
+        #TODO
+        pass
+    
+ #%% 
+"""Efficiency is for the amount of instructions a programm gets during a clockcylce
+Therefore, a more fit organism can do more in the same time"""  
+class Efficiency:
+    def __init__(self,value=0):
+        self.value = value
+    def raise_factor(self,rfactor):
+        self.value += rfactor
+    def lower_factor(self,lfactor):
+        self.value += lfactor
+        
+#%%
+"""Class for the input that we send to the world, ie. what we want our programms
+to do """
+class Input:
+    def init__(self,emulator):
+        self.emulator = emulator
+    
+        
