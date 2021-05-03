e4ab2864988f2cefea5ecfba779d4e1c08dd1cca
aleksandar-terzic <63882341+aleksandar-terzic@users.noreply.github.com>
2021-05-03 03:15:44 +0200

    Big update. Organisms are starting to evolve.

--- src/com/company/environment.py
+++ src/com/company/environment.py
@@ -4,9 +4,15 @@
 # Necessary imports:
 
 import numpy as np
-import SimpleAvida as SA
+import DigitalOrganism as DO
 from Mediator import Mediator
 import random
+import sys 
+
+#%% Helper class for breaking nested loops
+class BreakIt(Exception): pass
+
+
 
 #%% Pool simulates an NxN Petri dish
 # Avida organisms live in such a pool
@@ -52,54 +58,66 @@ class World(Mediator):
     
     # Ages keeps track of the ages of the organisms in the corresponding location
     
-    # mask is a helper variable which helps us quickly pull out a 1D list of all
-    # the valid populated positions in the world pool
     
     # Default copy mutation probability is 0.0025
     
-    def __init__(self, N, replacement_strategy = "kill_oldest", cm_prob = 0.0025, ins_prob = ):
+    def __init__(self, N, replacement_strategy = "neighborhood", cm_prob = 0.0025, ins_prob = 0.05, del_prob = 0.05):
 
         # Pool() will contain the set of CPUEmulators.
-        self.pool = Pool(N, dtype = SA.CPUEmulator)
+        self.pool = Pool(N, dtype = DO.CPUEmulator)
         
         self.rates = np.zeros((N,N), dtype = int)
         
-        self.replacement_strategy = replacement_strategy
-        
         self.ages = np.zeros((N,N), dtype = int)
-        
-        self.mask = np.zeros((N,N), dtype = bool)
-        
-        self.cm_prob = cm_prob
         
         self.inputs = np.empty((N,N))
         self.inputs[:] = np.nan
         
+        self.replacement_strategy = replacement_strategy
+        
+        self.cm_prob = cm_prob
+        
+        self.ins_prob = ins_prob
+        self.del_prob = del_prob
+        
+
     # The following method instantiates a default self-replicating organism
     # as a random location in the pool, unless location specified otherwise
     # The location must be a valid tuple
     
-    def instantiate_default(self, position = None):
+    # Default as per Avida-ED website and Nature paper. Contains 35 nop-c in the middle
+    # In total 50 instructions
+    
+    def place_default(self, position = None):
         
-        self.instantiate_custom([16, 20, 2, 0, 21, 2, 20, 19, 25, 2, 0, 17, 21, 0, 1], position = position)
+        self.place_custom([16, 20, 2, 0, 21] + [2]*36 + [20, 19, 25, 2, 0, 17, 21, 0, 1], position = position)
+        
+    # Default per avida paper, with only 13 instructions
+    def place_default_13(self, position = None):
+        
+        self.place_custom([16, 20, 2, 0, 21, 2, 20, 19, 25, 2, 0, 17, 21, 0, 1], position = position)
         
     # The following method instantiates a custom self-replicating organism
     # as a random location in the pool, unless location specified otherwise
     # The location must be a valid tuple
         
-    def instantiate_custom(self, program, position = None):
+    def place_custom(self, program, position = None):
         
-        # Create default program
-        default_program = SA.Program(program)
+        # Create program
+        default_program = DO.Program(program)
         
         # Create a new emulator
-        emulator = SA.CPUEmulator()
+        emulator = DO.CPUEmulator()
         
         # Load self as the emulator's mediator
         emulator.mediator = self
         
         # Set the copy mutation probability as defined in the world
         emulator.mutation_prob = self.cm_prob
+        
+        # Set the insertion and deletion mutation probabilities as defined in the world
+        emulator.ins_prob = self.ins_prob
+        emulator.del_prob = self.del_prob
         
         # Load program into emulator
         emulator.load_program(default_program)
@@ -118,7 +136,6 @@ class World(Mediator):
         
         self.pool.put(emulator,position)
         
-        
         # Put the emulator rate in the corresponding position
         # The world keeps track of the metabolic rates
         # Per default, upon any organism instantiation, the metabolic rate is 1
@@ -129,52 +146,61 @@ class World(Mediator):
         
         self.ages[position] = emulator.age
         
-        # Update mask
-        
-        self.mask[position] = True
-        
 
     # Here the scheduler loop, which runs forever unless told otherwise
     
     def schedule(self, n_loops = None):
         
         if n_loops == None:
-
+            
+            # I want to see how the population is doing every 10k cycles
+            
+            iterator = 0
             while True:
                 
-                baseline_rate = self.baseline_rate()
+                 baseline_rate = self.baseline_rate()
+                 iterator += 1
+                 
+                 if iterator == 10000:
+                     # Shows that the whole thing is alive every 10k cycles
+                     print(self.ages)
+                     iterator = 0
             
-                mask = self.mask
-            
-                emulators = self.pool.get()[mask]
-            
-                rates = self.rates
-
-                for i in range(emulators.size):
+                 for i in range(self.pool.shape()[0]):
+                     for j in range(self.pool.shape()[1]):
                     
-                    n_cycles = int(rates[mask][i] / baseline_rate)
-
-                    for i_cycles in range(n_cycles):
+                         if isinstance(self.pool.get()[i][j], DO.CPUEmulator):
                         
-                        emulators[i].execute_instruction()
+                             n_cycles = int(self.rates[i][j] / baseline_rate)
+                        
+                             for i_cycles in range(n_cycles):
+                            
+                                 current_emulator = self.pool.get()[i][j]
+                            
+                                 current_emulator.execute_instruction()
+                            
+                                 self.ages[i][j] = current_emulator.age
+                                
             
         for i_loops in range(n_loops):
-
+            
             baseline_rate = self.baseline_rate()
             
-            mask = self.mask
-            
-            emulators = self.pool.get()[mask]
-            
-            rates = self.rates
-
-            for i in range(emulators.size):
+            for i in range(self.pool.shape()[0]):
+                for j in range(self.pool.shape()[1]):
                     
-                    n_cycles = int(rates[mask][i] / baseline_rate)
-
-                    for i_cycles in range(n_cycles):
+                    if isinstance(self.pool.get()[i][j], DO.CPUEmulator):
                         
-                        emulators[i].execute_instruction()
+                        n_cycles = int(self.rates[i][j] / baseline_rate)
+                        
+                        for i_cycles in range(n_cycles):
+                            
+                            current_emulator = self.pool.get()[i][j]
+                            
+                            current_emulator.execute_instruction()
+                            
+                            self.ages[i][j] = current_emulator.age
+
 
     # The following method defines which functions are to be called when world is notified 
     # of various events
@@ -183,7 +209,7 @@ class World(Mediator):
         
         if event == "division":
             
-            self.react_on_division(result)
+            self.react_on_division(sender, result)
 
         elif event == "IO_operation":
 
@@ -194,16 +220,16 @@ class World(Mediator):
     
     # Here how the world reacts upon organism division
     
-    def react_on_division(self, result):
+    def react_on_division(self, sender, result):
         
         # The organism, upon valid division, notifies the world of it using
         # self.mediator.notify(sender = self, event = "division", result = result)
 
         # Create a program from the result passed from the organism which underwent division
-        program = SA.Program(result)
+        program = DO.Program(result)
         
         # Create a new emulator and load the resulting program in it
-        emulator = SA.CPUEmulator()
+        emulator = DO.CPUEmulator()
         emulator.load_program(program)
         
         # Link self as the new emulator's mediator
@@ -212,19 +238,66 @@ class World(Mediator):
         # Set the copy mutation probability as defined in the world
         emulator.mutation_prob = self.cm_prob
         
-        # If there is a free spot put the cell there
-        if 0 in self.pool.get():
+        # Default replacement strategy
+        # Look for free spots in the 1-hop neighborhood of the parent
+        # If there is a free spot, put the offspring into any such spot
+        # If not, kill the oldest organism in the neighborhood and put offspring there
+        # Note that the oldest organism in the neighborhood may be the parent itself
+        if self.replacement_strategy == "neighborhood":
             
-            idx0 = np.where(self.pool.get() == 0)[0][0]
-            idx1 = np.where(self.pool.get() == 0)[1][0]
+            # Find the position in the pool where the sender (parent) is at:
+
+            idx0 = np.where(self.pool.get() == sender)[0][0]
+            idx1 = np.where(self.pool.get() == sender)[1][0]
             
-            position = (idx0, idx1)
-        
-        # Otherwise find the oldest emulator and replace it with the newly created one
-        
+            width = self.pool.shape()[0]
+            height = self.pool.shape()[1]
+            
+            # The neighborhood of the cell:
+            # Iterate over rows max(idx0-1,0) idx0, min(idx0+1, pool height - 1)
+            # Iterate over columns max(idx1-1,0), idx1, min(idx1 + 1, pool width - 1)
+            
+            # Breaking out of nested loops: 
+            # https://stackoverflow.com/questions/653509/breaking-out-of-nested-loops
+            
+            position = None
+            
+            try:
+                for i in range(max(idx0-1, 0), min(idx0+2, width)):
+                    for j in range(max(idx1-1, 0), min(idx1+2, height)):  
+                        if self.pool.get()[i][j] == 0:
+                            position = (i,j)
+                            raise BreakIt
+                            
+            except BreakIt:
+                pass
+            
+            # If no free spot in the neighborhood was found, replace oldest cell in the neighborhood
+            if position == None:
+                
+                oldest = 0
+                
+                for i in range(max(idx0-1, 0), min(idx0+2, width)):
+                        for j in range(max(idx1-1, 0), min(idx1+2, height)):  
+                            if self.ages[i][j] >= oldest:
+                                oldest = self.ages[i][j]
+                                position = (i,j)
+
+        # Define kill_oldest replacement strategy as:
+        # If there is a free spot in the pool, put child there
+        # Otherwise, kill oldest organism in the pool and put child there
         elif self.replacement_strategy == "kill_oldest":
+            # If there is a free spot put the cell there
+            if 0 in self.pool.get():
             
-            position = self.oldest_position()
+                idx0 = np.where(self.pool.get() == 0)[0][0]
+                idx1 = np.where(self.pool.get() == 0)[1][0]
+            
+                position = (idx0, idx1)
+        
+            else:
+            
+                position = self.oldest_position()               
             
         # Put the created emulator in the found position
         
@@ -234,23 +307,24 @@ class World(Mediator):
         
         self.ages[position] = 0
         
-        # Update mask
-        
-        self.mask[position] = True
-        
         # Update rates
         
         self.rates[position] = 1
+        
+        # Set input to none
+        
+        self.inputs[position] = np.nan
             
     # Here how the world reacts upon an IO operation        
 
     def react_on_IO(self, sender, result):
         
         # Find the position in the pool where the sender is at:
+
+        idx0 = np.where(self.pool.get() == sender)[0][0]
+        idx1 = np.where(self.pool.get() == sender)[1][0]
         
-        idx0 = np.where(world.pool.get() == sender)[0][0]
-        idx1 = np.where(world.pool.get() == sender)[1][0]
-        
+        #
         if np.isnan(self.inputs[idx0][idx1]):
             
             pass
@@ -259,10 +333,7 @@ class World(Mediator):
             
             if self.inputs[idx0][idx1] == ~result:
                 
-                print("\nEMULATOR AT POSITION " + str((idx0,idx1)) + " COMPUTED NOT\n")
-                
-                # Doble the organism's metabolic rate
-                self.rates[(idx0,idx1)] *= 2
+                sys.exit("\nEMULATOR AT POSITION " + str((idx0,idx1)) + " COMPUTED NOT\n")
 
         # A random 32-bit number
         to_input = random.getrandbits(32)
@@ -281,12 +352,10 @@ class World(Mediator):
         # If there are several such emulators with equal age, returns the first one
         # it encounters
         
-        # The ages matrix is only updated when we need it
-        
         for i in range(0,self.pool.shape()[0]):
             for j in range(0,self.pool.shape()[1]):
                 
-                if isinstance(self.pool.get()[i][j], SA.CPUEmulator):
+                if isinstance(self.pool.get()[i][j], DO.CPUEmulator):
                     self.ages[i][j] = self.pool.get()[i][j].age
         
         idx0 = np.where(self.ages == np.max(self.ages))[0][0]
@@ -298,7 +367,7 @@ class World(Mediator):
         
         # Returns the smallest metabolic rate value present
         
-        baseline_rate = np.min(self.rates[self.mask])
+        baseline_rate = np.min(self.rates[self.rates > 0])
         
         return baseline_rate
     
@@ -306,12 +375,16 @@ class World(Mediator):
         
         return self.pool.get()
     
+    def get(self,position):
+        
+        return self.pool.get()[position]
+
     # A string representation of the world pool
 
     def __str__(self):
         
-        emulators = self.pool.get()[self.mask]
-        
+        emulators = self.pool.get()[self.pool.get() != 0]
+         
         for i in range(emulators.size):
 
             print("Emulator " + str(i + 1) + ": ")
@@ -319,12 +392,22 @@ class World(Mediator):
 
         return ""
 
+
 #%% Testing some stuff
 
-world = World(10)
+# First argument is world size
+# cm_prob is copy mutation probability
+# ins_prob and del_prob are insertion and deletion probabilities
+world = World(30, cm_prob = 0.0025, ins_prob = 0.05, del_prob = 0.05)
 
-world.instantiate_default((0,0))
-world.instantiate_default((1,0))
-world.instantiate_default((9,9))
+world.place_default((0,0))
+world.place_default((0,15))
+world.place_default((0,29))
+world.place_default((15,0))
+world.place_default((15,15))
+world.place_default((15,29))
+world.place_default((29,0))
+world.place_default((29,15))
+world.place_default((29,29))
 
-world.schedule(10000)
+world.schedule()
